902ef2fc29e01d1c15e6c4db2ec8558d030797d0
Sang Han <jjangsangy@gmail.com>
2020-01-08 01:17:12 -0800

    first commit

---
+++ Formula/ffmpeg.rb
@@ -0,0 +1,181 @@
+class Ffmpeg < Formula
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-4.2.2.tar.xz"
+  sha256 "cb754255ab0ee2ea5f66f8850e1bd6ad5cac1cd855d0a2f4990fb8c668b0d29c"
+  head "https://github.com/FFmpeg/FFmpeg.git"
+
+  # This formula is for people that will compile with their chosen options
+  bottle :unneeded
+
+  option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
+  option "with-decklink", "Enable DeckLink support"
+  option "with-fdk-aac", "Enable the Fraunhofer FDK AAC library"
+  option "with-librsvg", "Enable SVG files as inputs via librsvg"
+  option "with-libsoxr", "Enable the soxr resample library"
+  option "with-libssh", "Enable SFTP protocol via libssh"
+  option "with-tesseract", "Enable the tesseract OCR engine"
+  option "with-libvidstab", "Enable vid.stab support for video stabilization"
+  option "with-opencore-amr", "Enable Opencore AMR NR/WB audio format"
+  option "with-openh264", "Enable OpenH264 library"
+  option "with-openjpeg", "Enable JPEG 2000 image format"
+  option "with-openssl", "Enable SSL support"
+  option "with-rubberband", "Enable rubberband library"
+  option "with-webp", "Enable using libwebp to encode WEBP images"
+  option "with-zeromq", "Enable using libzeromq to receive cmds sent through a libzeromq client"
+  option "with-zimg", "Enable z.lib zimg library"
+  option "with-srt", "Enable SRT library"
+  option "with-libvmaf", "Enable libvmaf scoring library"
+  option "with-libxml2", "Enable libxml2 library"
+
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texi2html" => :build
+
+  depends_on "aom"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "gnutls"
+  depends_on "lame"
+  depends_on "libass"
+  depends_on "libvorbis"
+  depends_on "libvpx"
+  depends_on "opus"
+  depends_on "sdl2"
+  depends_on "snappy"
+  depends_on "x264"
+  depends_on "x265"
+  depends_on "xz"
+
+  depends_on "https://github.com/amiaopensource/homebrew-amiaos/blob/master/decklinksdk.rb" => :optional
+  depends_on "chromaprint" => :optional
+  depends_on "fdk-aac" => :optional
+  depends_on "game-music-emu" => :optional
+  depends_on "libbluray" => :optional
+  depends_on "libbs2b" => :optional
+  depends_on "libcaca" => :optional
+  depends_on "libgsm" => :optional
+  depends_on "libmodplug" => :optional
+  depends_on "librsvg" => :optional
+  depends_on "libsoxr" => :optional
+  depends_on "libssh" => :optional
+  depends_on "libvidstab" => :optional
+  depends_on "libvmaf" => :optional
+  depends_on "libxml2" => :optional
+  depends_on "opencore-amr" => :optional
+  depends_on "openh264" => :optional
+  depends_on "openjpeg" => :optional
+  depends_on "openssl" => :optional
+  depends_on "rtmpdump" => :optional
+  depends_on "rubberband" => :optional
+  depends_on "speex" => :optional
+  depends_on "srt" => :optional
+  depends_on "tesseract" => :optional
+  depends_on "two-lame" => :optional
+  depends_on "wavpack" => :optional
+  depends_on "webp" => :optional
+  depends_on "xvid" => :optional
+  depends_on "zeromq" => :optional
+  depends_on "zimg" => :optional
+
+  def install
+    # Work around Xcode 11 clang bug
+    # https://bitbucket.org/multicoreware/x265/issues/514/wrong-code-generated-on-macos-1015
+    ENV.append_to_cflags "-fno-stack-check" if DevelopmentTools.clang_build_version >= 1010
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --enable-nonfree
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --extra-cflags=-I#{HOMEBREW_PREFIX}/include
+      --extra-ldflags=-L#{HOMEBREW_PREFIX}/lib
+      --enable-avresample
+      --enable-ffplay
+      --enable-frei0r
+      --enable-gnutls
+      --enable-gpl
+      --enable-libaom
+      --enable-libass
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-libmp3lame
+      --enable-libopus
+      --enable-libsnappy
+      --enable-libtesseract
+      --enable-libtheora
+      --enable-libvidstab
+      --enable-libvorbis
+      --enable-libvpx
+      --enable-libx264
+      --enable-libx265
+      --enable-lzma
+      --enable-opencl
+      --enable-videotoolbox
+      --disable-indev=jack
+      --disable-libjack
+    ]
+
+    args << "--enable-chromaprint" if build.with? "chromaprint"
+    args << "--enable-decklink" if build.with? "decklink"
+    args << "--enable-libbluray" if build.with? "libbluray"
+    args << "--enable-libbs2b" if build.with? "libbs2b"
+    args << "--enable-libcaca" if build.with? "libcaca"
+    args << "--enable-libfdk-aac" if build.with? "fdk-aac"
+    args << "--enable-libgme" if build.with? "game-music-emu"
+    args << "--enable-libgsm" if build.with? "libgsm"
+    args << "--enable-libmodplug" if build.with? "libmodplug"
+    args << "--enable-libopenh264" if build.with? "openh264"
+    args << "--enable-librsvg" if build.with? "librsvg"
+    args << "--enable-librtmp" if build.with? "rtmpdump"
+    args << "--enable-librubberband" if build.with? "rubberband"
+    args << "--enable-libsoxr" if build.with? "libsoxr"
+    args << "--enable-libspeex" if build.with? "speex"
+    args << "--enable-libsrt" if build.with? "srt"
+    args << "--enable-libssh" if build.with? "libssh"
+    args << "--enable-libtesseract" if build.with? "tesseract"
+    args << "--enable-libtwolame" if build.with? "two-lame"
+    args << "--enable-libvidstab" if build.with? "libvidstab"
+    args << "--enable-libvmaf" if build.with? "libvmaf"
+    args << "--enable-libwavpack" if build.with? "wavpack"
+    args << "--enable-libwebp" if build.with? "webp"
+    args << "--enable-libxml2" if build.with? "libxml2"
+    args << "--enable-libxvid" if build.with? "xvid"
+    args << "--enable-libzimg" if build.with? "zimg"
+    args << "--enable-libzmq" if build.with? "zeromq"
+    args << "--enable-openssl" if build.with? "openssl"
+
+    if build.with? "openjpeg"
+      args << "--enable-libopenjpeg"
+      args << "--disable-decoder=jpeg2000"
+      args << "--extra-cflags=" + `pkg-config --cflags libopenjp2`.chomp
+    end
+
+    if build.with? "opencore-amr"
+      args << "--enable-libopencore-amrnb"
+      args << "--enable-libopencore-amrwb"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+
+    # Fix for Non-executables that were installed to bin/
+    mv bin/"python", pkgshare/"python", :force => true
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
---
+++ README.md
@@ -0,0 +1,92 @@
+# Homebrew Tap
+
+Personal homebrew taps
+
+# Installation
+
+```sh
+$ brew tap-pin jjangsangy/packages
+$ brew install ffmpeg
+```
+
+In order to uninstall run
+
+```sh
+$ brew untap-pin jjangsangy/packages
+```
+
+# Options
+
+```sh
+$ brew options ffmpeg --compact
+```
+
+## Included libraries
+
+This formula installs the following libraries by default:
+
+| Dependency   | Description                |
+|--------------|----------------------------|
+| `libaom`     | AV1 encoder                |
+| `fontconfig` | Font access library        |
+| `freetype`   | Freetype font engine       |
+| `frei0r`     | frei0r filters             |
+| `lame`       | LAME MP3 encoder           |
+| `libass`     | ASS subtitle support       |
+| `libvorbis`  | Vorbis encoder             |
+| `libvpx`     | VP8 and VP9 encoder        |
+| `opus`       | Opus encoder               |
+| `rtmpdump`   | RTMP dumping support       |
+| `sdl2`       | Simple DirectMedia Layer   |
+| `snappy`     | Snappy compression support |
+| `theora`     | Theora encoder             |
+| `x264`       | x264 H.264 encoder         |
+| `x265`       | x265 HEVC encoder          |
+| `xz`         | XZ compression support     |
+
+
+This formula features the following libraries optionally:
+
+| **Dependency**   | **Description**                                               |
+|------------------|---------------------------------------------------------------|
+| `chromaprint`    | Chromaprint audio fingerprinting library                      |
+| `decklink`       | Enable DeckLink support <sup>[[1]](#1)</sup>           |
+| `fdk-aac`        | Fraunhofer FDK AAC library                                    |
+| `game-music-emu` | game-music-emu support                                        |
+| `libbluray`      | libbluray support                                             |
+| `libbs2b`        | libbs2b support                                               |
+| `libcaca`        | libcaca support                                               |
+| `libgsm`         | libgsm support                                                |
+| `libmodplug`     | libmodplug support                                            |
+| `librsvg`        | SVG files as inputs via librsvg                               |
+| `libsoxr`        | soxr resample library                                         |
+| `libssh`         | SFTP protocol via libssh                                      |
+| `libvidstab`     | vid.stab support for video stabilization                      |
+| `libvmaf`        | VMAF video quality metric                                     |
+| `libxml2`        | libxml2 library                                               |
+| `opencore-amr`   | Opencore AMR NR/WB audio format                               |
+| `openh264`       | OpenH264 library                                              |
+| `openjpeg`       | JPEG 2000 image format                                        |
+| `openssl`        | SSL support                                                   |
+| `rtmpdump`       | rtmpdump support                                              |
+| `rubberband`     | rubberband library                                            |
+| `speex`          | speex support                                                 |
+| `srt`            | SRT library                                                   |
+| `tesseract`      | tesseract OCR engine                                          |
+| `two-lame`       | two-lame support                                              |
+| `wavpack`        | wavpack support                                               |
+| `webp`           | libwebp to encode WEBP images                                 |
+| `xvid`           | xvid support                                                  |
+| `zeromq`         | libzeromq to receive commands sent through a libzeromq client |
+| `zimg`           | z.lib zimg library                                            |
+
+
+# Reference
+
+
+<span id="1">_[1]_:</span>
+The DeckLink SDK has to be installed before running the FFmpeg formula.
+One possibility is to use
+```sh
+$ brew install amiaopensource/amiaos/decklinksdk
+```
